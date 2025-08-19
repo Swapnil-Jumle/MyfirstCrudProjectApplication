c8e6a32ee0c172ad540f7aa58b33d6c5393380cc
Dell <swapniljumle12345@gmail.com>
2025-08-19 22:51:51 +0530

    IntellijFirst

---
+++ src/main/java/com/company/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import com.company.exception.DeveloperNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GlobalExceptionHandler {
+    @ExceptionHandler(DeveloperNotFoundException.class)
+    public ResponseEntity<String> handleDeveloperNotFoundException(DeveloperNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage() , HttpStatus.NOT_FOUND);
+    }
+}
